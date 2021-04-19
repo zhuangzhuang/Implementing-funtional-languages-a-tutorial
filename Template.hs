f2c46ffcce6632e6423c8a9577a9b822d57f6ec8
zhuangzhuang1988 <zhuangzhuang1988@outlook.com>
2021-04-19 17:22:52 +0800

    mult

--- Template.hs
+++ Template.hs
@@ -1,3 +1,26 @@
 module Template where
+
 import Language
 import Utils
+
+-- n*m
+-- (n m d t)
+-- n m  d-1 t+1
+-- where d > 0
+
+type MultState = (Int, Int, Int, Int)
+
+stepMult :: MultState -> MultState
+stepMult (n, m, d, t) | d > 0 = (n, m, d -1, t + 1)
+stepMult (n, m, d, t) | d == 0 = (n, m -1, n, t)
+
+mutltFinal :: MultState -> Bool
+mutltFinal (n, m, d, t)
+  | m == 0 && d == 0 = True
+  | otherwise = False
+
+evalMul :: MultState -> [MultState]
+evalMul state =
+  if mutltFinal state
+    then [state]
+    else state : evalMul (stepMult state)
