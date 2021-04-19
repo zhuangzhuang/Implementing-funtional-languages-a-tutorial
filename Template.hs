6143c983c92596a7a6b234983553b1118275511a
zhuangzhuang1988 <zhuangzhuang1988@outlook.com>
2021-04-19 17:32:39 +0800

    stats

--- Template.hs
+++ Template.hs
@@ -24,3 +24,51 @@ evalMul state =
   if mutltFinal state
     then [state]
     else state : evalMul (stepMult state)
+
+--  (stack, dump, heap, globals)
+-- statk - stack of address
+--
+
+data Node
+  = NAp Addr Addr
+  | NSupercomb Name [Name] CoreExpr
+  | NNum Int
+
+type TiGlobals = ASSOC Name Addr
+
+type TiStats = Int
+
+tiStatInitial :: TiStats
+tiStatInitial = 0
+
+tiStatIncSetps :: TiStats -> TiStats
+tiStatIncSetps s = s + 1
+
+tiStatGetSetps :: TiStats -> Int
+tiStatGetSetps s = s
+
+type TiStack = [Addr]
+
+data TiDump = DummyTiDump
+
+initialTiDump = DummyTiDump
+
+type TiHeap = Heap Node
+
+type TiState = (TiStack, TiDump, TiHeap, TiGlobals, TiStats)
+
+applyToStats :: (TiStats -> TiStats) -> TiState -> TiState
+applyToStats stats_fun (stack, dump, heap, sc_defs, stats) =
+  (stack, dump, heap, sc_defs, stats_fun stats)
+
+compile :: CoreProgram -> TiState
+compile = error "asdf"
+
+eval :: TiState -> [TiState]
+eval = error "asdf"
+
+showResults :: [TiState] -> String
+showResults = error "asdf"
+
+runProg :: String -> String
+runProg = showResults . eval . compile . parse
